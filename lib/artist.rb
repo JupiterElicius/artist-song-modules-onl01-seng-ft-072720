23e3f2833b474c97f649e146f2e98d58d96cfc2b
Nick Bazaldua <bazalnick@gmail.com>
2020-08-23 16:11:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,8 +4,8 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   extend Memorable::ClassMethods
-  include Memorable::ClassMethods
   extend Findable::ClassMethods
+  include Memorable::InstanceMethods
   include Paramable::InstanceMethods
   @@artists = []
 
